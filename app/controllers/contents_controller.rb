3ba845d1c9254c8595ce1992d2bb8bd4acc5789e
Paulo202005 <paulo.argenton@gmail.com>
2021-03-30 21:56:39 -0300

    Permite adicionar tag nos conteúdos
    Tarefa: https://trello.com/c/aQfG1dX4/11-permitir-adicionar-tag-nos-conte%C3%BAdos

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -16,6 +16,7 @@ class ContentsController < ApplicationController
     @content = current_user.contents.build(content_params)
 
     if @content.save
+      associate_tags!
       redirect_to contents_path, notice: 'Content successfully created!'
     else
       render :new
@@ -26,6 +27,7 @@ class ContentsController < ApplicationController
 
   def update
     if @content.update(content_params)
+      associate_tags!
       redirect_to contents_path, notice: 'Content successfully updated!'
     else
       render :edit
@@ -34,7 +36,6 @@ class ContentsController < ApplicationController
 
   def destroy    
     @content.destroy
-
     redirect_to contents_path, notice: 'Content successfully destroyed!'
   end
 
@@ -44,7 +45,19 @@ class ContentsController < ApplicationController
     @content = Content.find(params[:id])
   end
 
+  def tags_params
+    params.require(:content).permit(tags: [])[:tags].reject(&:blank?)
+  end
+
   def content_params
     params.require(:content).permit(:title, :description)
+  end
+
+  def associate_tags!
+    tags = tags_params.map do |tag_name|
+      current_user.tags.where(name: tag_name).first_or_initialize
+    end
+
+    @content.tags = tags
   end
 end
